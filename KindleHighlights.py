d809ee132f2de2749ad0a763d5a8b9b73053fa4b
Vali-Boi <60585254+Vali-Boi@users.noreply.github.com>
2020-05-24 21:17:52 +0200

    Regex working

--- KindleHighlights.py
+++ KindleHighlights.py
@@ -3,24 +3,38 @@ import re
 import pprint
 
 # E:\documents\My Clippings.txt (?<=={10}\n
-# for the title(?<=={10}\n-\s)
+# for the title(?<=={10}\n).+\n
+# for the quotes (?<=\n\n).*(?=\n={10})
+
+
+# with open(r"C:\Users\Valdrin\Desktop\My Clippings.txt", encoding="utf-8", mode="r+") as clippings:
+#    content = clippings.read
+#    if content[0] != "﻿==========\n":
+#
+#        content.seek(0)
+#        content.write("==========\n")
+#        print("shit has been added")
+
 
 # get the "My Clippings" text document
-with open(r"C:\Users\Valdrin\Desktop\My Clippings.txt", "rb") as clippings:
-    content = clippings.read().decode('utf-8')
-    print(content)
+with open(r"C:\Users\Valdrin\Desktop\My Clippings.txt", encoding="utf-8") as clippings:
+    content = clippings.read()
     # match the title (maybe author)
-    titleRegex = re.compile(r'(?<=={10}\n-\s)')
+    titleRegex = re.compile(r'(?<=={10}\n).+\n')
     title_match = re.findall(titleRegex, content)
-    print(title_match)
+
     # match the quote
-    quoteRegex = re.compile(r"(?<=\n\n).+(?=\n={10})")
+    quoteRegex = re.compile(r"(?<=\n\n).*(?=\n={10})")
     quote_match = re.findall(quoteRegex, content)
-    print(quote_match)
     title_quote_list = (list(zip(title_match, quote_match)))
 
 '''Filtering the title_quote_list into a dict with the titles as the key and a list of corresponding
    quotes as the value'''
+
+title_quote_list = [comb for comb in title_quote_list if comb[1] != ""]
+
+# for comb in title_quote_list:
+#     print(comb)
 
 main_dict = {}
 for title in title_match:
@@ -28,24 +42,21 @@ for title in title_match:
 
 for item in title_quote_list:
     main_dict[item[0]].append(item[1])
-# print(main_dict)
 
-someArray = []
+for key, value in main_dict.items():
+    print(key, value)
+
 for quotes in main_dict.values():
-    for quote in quotes:
-        pass
+    for i in range(len(quotes)-1):
+        j = i + 1
+        if j > len(quotes)-1:
+            break
+        if quotes[i] in quotes[j] or quotes[j] in quotes[i]:
+            if len(quotes[i]) < len(quotes[j]):
+                del quotes[i]
+            else:
+                del quotes[j]
 
 
-
-
-    # index = someArray.index(max(someArray))
-    # new = quotes[index]
-    # quotes.clear()
-    # quotes.append(new)
-
-
-
-
-# for title, quotes in main_dict.items():
-#     for quote in quotes:
-#         print(f"{title}\n{quote}")
+for key, value in main_dict.items():
+    print(key, value)
--- test.py
+++ test.py
@@ -1,18 +1,35 @@
-def solve(arr):
-    workarr = []
-    i = 0
-    while arr[0:2] != [0,0]:
-        for x in range(2):
-            arr = sorted(arr)
-            ind = (arr.index(max(arr)))
-            bla = arr.pop(ind) - 1
-            workarr.append(bla)
-        arr.extend(workarr)
-        arr = sorted(arr)
-        i += 1
-        workarr = []
-    return i
+def increment_string(strng):
+    stri = list(strng)
+    num_list = []
+    str_list = []
+    for i in stri:
+        if i in ["1", "2", "3", "4", "5", "6", "7", "8", "9"]:
+            num_list.append(i)
+        else:
+            str_list.append(i)
 
 
-print(solve([423,134,123]))
+    if num_list == []:
+        if str_list[-1] == "0":
+            strng = strng[:-1]
+            return strng + "1"
+        else:
+            return strng + "1"
+    num_str_before = "".join(num_list)
+    num_before_length = len(num_str_before)
+    num = int(num_str_before) + 1
+    num_str_after = str(num)
+    num_str_after_length = len(num_str_after)
+    if num_before_length == num_str_after_length:
+        str_list.append(num_str_after)
+        return "".join(str_list)
+    else:
+        if str_list[-1] == "0":
+            str_list = str_list[:-1]
+        str_list.append(num_str_after)
+        return "".join(str_list)
+
+
+print(increment_string("foobar001"))
+
 
